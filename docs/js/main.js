f72061f8a19ca0df2a354d3d915884748563c523
gladjanus43 <37533884+gladjanus43@users.noreply.github.com>
2018-06-20 21:01:24 +0200

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,172 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var GameObject = (function () {
+    function GameObject(x, y) {
+        this.posX = 0;
+        this.posY = 0;
+        this.posX = x;
+        this.posY = y;
+    }
+    return GameObject;
+}());
+var Ball = (function (_super) {
+    __extends(Ball, _super);
+    function Ball(x, y) {
+        var _this = _super.call(this, x, y) || this;
+        _this.element = document.createElement('ball');
+        document.body.appendChild(_this.element);
+        window.addEventListener("mousemove", _this.mouseMovement.bind(_this));
+        return _this;
+    }
+    Ball.prototype.mouseMovement = function (e) {
+        this.posX = e.clientX;
+        this.posY = e.clientY;
+    };
+    Ball.prototype.move = function () {
+        this.element.style.transform = 'translate(' + this.posX + 'px, ' + this.posY + 'px)';
+    };
+    Ball.prototype.getRectangle = function () {
+        return this.element.getBoundingClientRect();
+    };
+    return Ball;
+}(GameObject));
+var Game = (function () {
+    function Game() {
+        console.log('constr');
+        this.ball = new Ball(300, 200);
+        this.paddles = [];
+        requestAnimationFrame(this.gameLoop.bind(this));
+        this.paddles.push(new Paddle(this.randomX(), window.innerHeight));
+        this.interval = 20;
+        window.addEventListener('click', this.buttonClickEvent.bind(this));
+        this.counter = 0;
+        this.hasHit = false;
+        this.score = 0;
+        this.prevScore = this.getScore();
+        this.scoreMenu = document.createElement('score');
+        this.setPreviousScoreItem();
+    }
+    Game.prototype.randomNumber = function (begin, end) {
+        var ran = Math.floor(Math.random() * end + begin);
+        return ran;
+    };
+    Game.prototype.gameLoop = function () {
+        if (this.hasHit == false) {
+            this.ball.move();
+            for (var i = 0; i < this.paddles.length; i++) {
+                this.paddles[i].move();
+            }
+            this.removePaddles();
+            this.checkAllCollisions();
+            if (this.counter >= this.interval) {
+                this.createPaddles();
+                this.counter = 0;
+            }
+            this.counter++;
+            this.interval -= 0.01;
+        }
+        this.scorePoints();
+        requestAnimationFrame(this.gameLoop.bind(this));
+    };
+    Game.prototype.checkAllCollisions = function () {
+        var hit;
+        for (var i = 0; i < this.paddles.length; i++) {
+            hit = this.checkCollision(this.paddles[i].getRectangle(), this.ball.getRectangle());
+            if (hit == true) {
+                this.hasHit = true;
+                this.showScore();
+                if (this.prevScore < this.score) {
+                    this.setScore(this.score);
+                }
+            }
+        }
+    };
+    Game.prototype.checkCollision = function (a, b) {
+        return (a.left <= b.right &&
+            b.left <= a.right &&
+            a.top <= b.bottom &&
+            b.top <= a.bottom);
+    };
+    Game.prototype.removePaddles = function () {
+        if (this.paddles[0].getPosY() <= -120) {
+            this.paddles[0].removeElement();
+            this.paddles.splice(0, 1);
+        }
+    };
+    Game.prototype.createPaddles = function () {
+        this.paddles.push(new Paddle(this.randomX(), window.innerHeight));
+    };
+    Game.prototype.randomX = function () {
+        return Math.random() * (window.innerWidth - 200);
+    };
+    Game.prototype.scorePoints = function () {
+        if (this.hasHit == false) {
+            this.score += 1;
+        }
+    };
+    Game.prototype.showScore = function () {
+        document.body.appendChild(this.scoreMenu);
+        var span = document.createElement('span');
+        this.scoreMenu.appendChild(span);
+        span.innerHTML = this.score.toString();
+        var button = document.createElement('button');
+        button.innerHTML = 'restart';
+        this.scoreMenu.appendChild(button);
+    };
+    Game.prototype.setScore = function (score) {
+        localStorage.setItem('score', score.toString());
+    };
+    Game.prototype.getScore = function () {
+        return localStorage.getItem('score');
+    };
+    Game.prototype.setPreviousScoreItem = function () {
+        var prev = document.getElementById('score');
+        prev.innerHTML = this.prevScore;
+    };
+    Game.prototype.buttonClickEvent = function (e) {
+        if (e.target.nodeName == 'BUTTON') {
+            this.restart();
+        }
+    };
+    Game.prototype.restart = function () {
+        window.location.reload();
+    };
+    return Game;
+}());
+window.onload = function () {
+    new Game;
+};
+var Paddle = (function (_super) {
+    __extends(Paddle, _super);
+    function Paddle(x, y) {
+        var _this = _super.call(this, x, y) || this;
+        _this.element = document.createElement('paddle');
+        document.body.appendChild(_this.element);
+        _this.element.style.transform = 'translate(' + _this.posX + 'px, ' + _this.posY + 'px)';
+        return _this;
+    }
+    Paddle.prototype.move = function () {
+        this.posY -= 10;
+        this.element.style.transform = 'translate(' + this.posX + 'px, ' + this.posY + 'px)';
+    };
+    Paddle.prototype.getPosY = function () {
+        return this.posY;
+    };
+    Paddle.prototype.getRectangle = function () {
+        return this.element.getBoundingClientRect();
+    };
+    Paddle.prototype.removeElement = function () {
+        return this.element.remove();
+    };
+    return Paddle;
+}(GameObject));
+//# sourceMappingURL=main.js.map
